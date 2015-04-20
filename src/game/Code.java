9549554524e969cfe658f9a5824b29cf8ca44ce6
ps90 <ps90@st-andrews.ac.uk>
2015-04-20 14:31:38 +0100

    a few changes

--- src/game/Code.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-/**
- * Created by ps90 on 19/04/15.
- */
-public enum Code {
-    GLISTEN, BREEZE, STENCH, EXIT, TREASURE, SHOOT, MOVE, CONNECTIONS, PIT, WUMPUS, SHOTSELECTION, IGNORE
-}
