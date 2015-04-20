90fbf97d97b69c7485c6c100daac0375d5579b92
ps90 <ps90@st-andrews.ac.uk>
2015-04-20 17:32:05 +0100

    nothing

--- src/game/IConstants.java
+++ src/game/IConstants.java
@@ -7,8 +7,8 @@ public interface IConstants {
     /*
     int MAX_ADJACENT_CAVES = 5;
     */
-    int NUMBER_OF_CAVES = 30;
-    int NUMBER_OF_BATS = 0;
+    int NUMBER_OF_CAVES = 10;
+    int NUMBER_OF_BATS = 1;
     int NUMBER_OF_PITS = 1;
     int INITIAL_ARROWS = 5;
     
@@ -17,7 +17,7 @@ public interface IConstants {
     final static int soTimeout = 10; // ms to wait for socket read
     final static int readRetry = 10; // # re-try of handshake
     final static int sleepTime = 200; // ms to sleep - 200 is fine
-    final static int bufferSize = 128; // # chars in line
+    final static int bufferSize = 512; // # chars in line
     
 	// network messages
 	String COMMAND_FORFEIT = "you-win";
